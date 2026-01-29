358c6e45740d4213e703eef1a1467a16ef05954b
Cynthia Tristán <cyntrist@ucm.es>
2026-01-29 01:23:40 +0100

    part 3.2

--- GodotProject/Scripts/PreProduction/BoardCreator.gd
+++ GodotProject/Scripts/PreProduction/BoardCreator.gd
@@ -80,11 +80,10 @@ func ShrinkArea():
 	var r: Rect2i = _RandomRect()
 	_ShrinkRect(r)
 
-func Save():
-	var saveFile: String = savePath + fileName
-	var save_game: FileAccess = FileAccess.open(saveFile, FileAccess.WRITE)
-	var version: int = 1
-	var size: int = tiles.size()
+func SaveMap(saveFile):
+	var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
+	var version = 1
+	var size = tiles.size()
 	
 	save_game.store_8(version)
 	save_game.store_16(size)
@@ -96,21 +95,20 @@ func Save():
 		
 	save_game.close()
 
-func Load() -> void:
-	Clear()
-	
-	var saveFile: String = savePath + fileName
+func LoadMap(saveFile):
+	Clear()	
+
 	if not FileAccess.file_exists(saveFile):
 		return # Error! We don't have a save to load.
 		
-	var save_game: FileAccess = FileAccess.open(saveFile, FileAccess.READ)
-	var version: int = save_game.get_8()
-	var size: int = save_game.get_16()
+	var save_game = FileAccess.open(saveFile, FileAccess.READ)
+	var version = save_game.get_8()
+	var size = save_game.get_16()
 	
 	for i in range(size):
-		var save_x: int = save_game.get_8()
-		var save_z: int = save_game.get_8()
-		var save_height: int = save_game.get_8()
+		var save_x = save_game.get_8()
+		var save_z = save_game.get_8()
+		var save_height = save_game.get_8()
 		
 		var t: Tile = _Create()
 		t.Load(Vector2i(save_x, save_z) , save_height)
@@ -119,47 +117,42 @@ func Load() -> void:
 	save_game.close()
 	_UpdateMarker()
 
-func SaveJSON():
-	var main_dict: Dictionary[Variant, Variant] = {
+func SaveMapJSON(saveFile):
+	var main_dict = {
 		"version": "1.0.0",
 		"tiles": []
 	}
 		
 	for key in tiles:
-		var save_dict: Dictionary[Variant, Variant] = {
+		var save_dict = {
 			"pos_x" : tiles[key].pos.x,
 			"pos_z" : tiles[key].pos.y,
 			"height" : tiles[key].height
 			}
 		main_dict["tiles"].append(save_dict)
 
-	#var saveFile = savePath + fileName
-	#var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
-	var save_game: FileAccess = FileAccess.open("res://Data/Levels/savegame.json", FileAccess.WRITE)
+	var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
 	
-	var json_string: String = JSON.stringify(main_dict, "\t", false)
+	var json_string = JSON.stringify(main_dict, "\t", false)
 	save_game.store_line(json_string)
 
-func LoadJSON() -> void:
+func LoadMapJSON(saveFile):
 	Clear()
 
-	#var saveFile = savePath + fileName
-	#if not FileAccess.file_exists(saveFile):
-	if not FileAccess.file_exists("res://Data/Levels/savegame.json"):
+	if not FileAccess.file_exists(saveFile):
 		return # Error! We don't have a save to load.
-	
-	#var save_game = FileAccess.open(saveFile, FileAccess.READ)	
-	var save_game: FileAccess = FileAccess.open("res://Data/Levels/savegame.json", FileAccess.READ)	
+		
+	var save_game = FileAccess.open(saveFile, FileAccess.READ)	
 
-	var json_text: String = save_game.get_as_text()	
-	var json: JSON = JSON.new()
-	var parse_result: int = json.parse(json_text)
+	var json_text = save_game.get_as_text()	
+	var json = JSON.new()
+	var parse_result = json.parse(json_text)
 
 	if parse_result != OK:
 		print("Error %s reading json file." % parse_result)
 		return
 		
-	var data: Dictionary[Variant, Variant] = {}
+	var data = {}
 	data = json.get_data()
 
 	for mtile in data["tiles"]:
@@ -169,6 +162,26 @@ func LoadJSON() -> void:
 	
 	save_game.close()
 	_UpdateMarker()
+	
+func Save():
+	var saveFile = savePath + fileName
+	SaveMap(saveFile)
+
+func Load():
+	var saveFile = savePath + fileName
+	LoadMap(saveFile)
+
+func SaveJSON():
+	#var saveFile = savePath + fileName
+	#var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
+	var saveFile = "res://Data/Levels/savegame.json"
+	SaveMapJSON(saveFile)
+
+func LoadJSON():
+	#var saveFile = savePath + fileName
+	#var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
+	var saveFile = "res://Data/Levels/savegame.json"
+	LoadMapJSON(saveFile)
 
 func _ready():
 	marker = tileSelectionIndicatorPrefab.instantiate()
